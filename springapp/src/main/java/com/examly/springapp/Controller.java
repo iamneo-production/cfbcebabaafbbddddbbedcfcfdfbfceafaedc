136cc4587aa3d34bd97e86ee6e41f4b0daae39ff
Iamneo <techadmin@iamneo.ai>
2023-08-14 08:09:17 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/Controller.java
+++ springapp/src/main/java/com/examly/springapp/Controller.java
@@ -1,9 +1,11 @@
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Controller {
     @RequestMapping("/")
+    @ResponseBody
     public String helloWorld() {
         return "Hello World from Spring Boot";
     }
